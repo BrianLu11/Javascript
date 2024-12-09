efc3cca4122565ac40540b29fc36c79b475079a5
BrianLu11 <luhsuehhao@gmail.com>
2024-12-09 15:42:13 +0800

    Add files via upload

---
+++ imag.js
@@ -0,0 +1,13 @@
+// Picture Rotation
+var arr = [
+    'https://images.unsplash.com/photo-1499678329028-101435549a4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
+    'https://images.unsplash.com/photo-1479030160180-b1860951d696?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
+    'https://images.unsplash.com/photo-1507608869274-d3177c8bb4c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+];
+
+var intervalId = window.setInterval(function(){
+    var elem = document.getElementById('head');    
+    j = i % arr.length
+    elem.src = arr[j];
+    return i++
+}, 5000);
